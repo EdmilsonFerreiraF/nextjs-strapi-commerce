85b1b0079c8f243e8ee7944e758c8d1d7c0feca1
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-02-02 12:58:28 -0300

    🚀 :rocket: [Add] Nav's Signup, Login buttons

--- components/nav.js
+++ components/nav.js
@@ -1,7 +1,12 @@
-import React from "react"
+import React, { useContext } from "react"
 import Link from "next/link"
+import { Nav as NavList, NavItem } from "reactstrap";
+import { logout } from "../lib/auth";
+import GlobalContext from "../context/GlobalContext";
 
 const Nav = ({ categories }) => {
+  const { user, setUser } = useContext(GlobalContext);
+
   return (
     <div>
       <nav className="uk-navbar-container" data-uk-navbar>
@@ -25,6 +30,34 @@ const Nav = ({ categories }) => {
                 </li>
               )
             })}
+              <NavItem className="ml-auto">
+                {user ? (
+                  <h5>{user.username}</h5>
+                ) : (
+                  <Link href="/register">
+                    <a className="nav-link"> Sign up</a>
+                  </Link>
+                )}
+              </NavItem>
+              <NavItem>
+                {user ? (
+                  <Link href="/">
+                    <a
+                      className="nav-link"
+                      onClick={() => {
+                        logout();
+                        setUser(null);
+                      }}
+                    >
+                      Logout
+                    </a>
+                  </Link>
+                ) : (
+                  <Link href="/login">
+                    <a className="nav-link">Sign in</a>
+                  </Link>
+                )}
+              </NavItem>
           </ul>
         </div>
       </nav>
