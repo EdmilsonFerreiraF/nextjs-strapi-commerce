fcefc30587086a701676141283f71495984263f4
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-02-05 09:32:09 -0300

    📝 :pencil: [Minor] Retrieve payment

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,33 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
-
-import CheckoutForm from "../components/checkout/CheckoutForm";
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
-
-// Make sure to call loadStripe outside of a component’s render to avoid
-// recreating the Stripe object on every render.
-// This is your test publishable API key.
-const stripePromise = loadStripe("pk_test_51KOP3uHbz4bukEWOFwNbJHuIKt4wYew0Pt761J42BuD7Kwu9aNR14c9HW20fZYvJ7fsxOzeKbBvRWHxZL6reHAWH00GpdQjqkg");
-
-export default function App() {
-  const [clientSecret, setClientSecret] = useState("");
-
-  useEffect(() => {
-    // Create PaymentIntent as soon as the page loads
-    fetch(`${API_URL}/api/orders`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
-    })
-      .then((res) => res.json())
-      .then((data) => setClientSecret(data.clientSecret));
-  }, []);
-
-
+const Checkout = () => {
   return (
     <div className="App">
-
     </div>
   );
 }
+
+export default Checkout
