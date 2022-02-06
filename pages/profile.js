77c36fae251af5019c8651c7727caa227345b190
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-02-05 23:16:33 -0300

    📝 :pencil: [Minor] Bought products tab

--- pages/profile.js
+++ pages/profile.js
@@ -6,7 +6,7 @@ const Profile = ({ categories }) => {
         <Layout categories={categories}>
             <div className="container-md">
                 <h1>My profile</h1>
-                <ul class="nav nav-pills">
+                <ul class="nav nav-pills my-5">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Bought</a>
                     </li>
@@ -20,7 +20,39 @@ const Profile = ({ categories }) => {
                         <a class="nav-link disabled">Disabled</a>
                     </li>
                 </ul>
+
+                <div class="list-group w-100 container-md my-5">
+                <a href="#" class="list-group-item list-group-item-action d-flex align-items-center active" aria-current="true">
+                    <input class="form-check-input me-3" type="checkbox" value="" />
+                    <div>
+                        <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">List group item heading</h5>
+                            <small>3 days ago</small>
+                        </div>
+                        <p class="mb-1">Some placeholder content in a paragraph.</p>
+                        <small>And some small print.</small>
+                    </div>
+                </a>
+                <a href="#" class="list-group-item list-group-item-action">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">List group item heading</h5>
+                        <small class="text-muted">3 days ago</small>
+                    </div>
+                    <p class="mb-1">Some placeholder content in a paragraph.</p>
+                    <small class="text-muted">And some muted small print.</small>
+                </a>
+                <a href="#" class="list-group-item list-group-item-action">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">List group item heading</h5>
+                        <small class="text-muted">3 days ago</small>
+                    </div>
+                    <p class="mb-1">Some placeholder content in a paragraph.</p>
+                    <small class="text-muted">And some muted small print.</small>
+                </a>
             </div>
+            </div>
+
+           
         </Layout>
     )
 }
