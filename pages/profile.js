579d32ef922dca0a71622a28d3648915a81e0ee3
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-02-06 11:53:12 -0300

    📝 :pencil: [Minor] Switch profile tabs

--- pages/profile.js
+++ pages/profile.js
@@ -1,89 +1,96 @@
+import { useState } from "react"
 import Layout from "../components/layout"
 import { fetchAPI } from "../lib/api"
 
 const Profile = ({ categories }) => {
+    let [activeTab, setActiveTab] = useState(0)
+
+    const handleActiveTab = (tab) => {
+        setActiveTab(tab)
+    }
+    
     return (
         <Layout categories={categories}>
             <div className="container-md">
                 <h1>My profile</h1>
-                <ul class="nav nav-pills my-5">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Bought</a>
+                <ul className="nav nav-pills my-5">
+                    <li className="nav-item">
+                        <a className={`nav-link${!activeTab ? " active" : ""}`} onClick={() => handleActiveTab(0)} aria-current="page" href="#">Bought</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Favorites</a>
+                    <li className="nav-item">
+                        <a className={`nav-link${activeTab === 1 ? " active" : ""}`} onClick={() => handleActiveTab(1)} href="#">Favorites</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                    <li className="nav-item">
+                        <a className={`nav-link${activeTab === 2 ? " active" : ""}`} onClick={() => handleActiveTab(2)} href="#">Link</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled">Disabled</a>
+                    <li className="nav-item">
+                        <a className={`nav-link${activeTab === 3 ? " active" : ""}`} onClick={() => handleActiveTab(3)}>Disabled</a>
                     </li>
                 </ul>
 
-                <div class="list-group w-100 container-md my-5">
-                    <a href="#" class="list-group-item list-group-item-action d-flex align-items-center active" aria-current="true">
-                        <input class="form-check-input me-3" type="checkbox" value="" />
+                <div className="list-group w-100 container-md my-5">
+                    <a href="#" className="list-group-item list-group-item-action d-flex align-items-center active" aria-current="true">
+                        <input className="form-check-input me-3" type="checkbox" value="" />
                         <div>
-                            <div class="d-flex w-100 justify-content-between">
-                                <h5 class="mb-1">List group item heading</h5>
+                            <div className="d-flex w-100 justify-content-between">
+                                <h5 className="mb-1">List group item heading</h5>
                                 <small>3 days ago</small>
                             </div>
-                            <p class="mb-1">Some placeholder content in a paragraph.</p>
+                            <p className="mb-1">Some placeholder content in a paragraph.</p>
                             <small>And some small print.</small>
                         </div>
                     </a>
-                    <a href="#" class="list-group-item list-group-item-action">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h5 class="mb-1">List group item heading</h5>
-                            <small class="text-muted">3 days ago</small>
+                    <a href="#" className="list-group-item list-group-item-action">
+                        <div className="d-flex w-100 justify-content-between">
+                            <h5 className="mb-1">List group item heading</h5>
+                            <small className="text-muted">3 days ago</small>
                         </div>
-                        <p class="mb-1">Some placeholder content in a paragraph.</p>
-                        <small class="text-muted">And some muted small print.</small>
+                        <p className="mb-1">Some placeholder content in a paragraph.</p>
+                        <small className="text-muted">And some muted small print.</small>
                     </a>
-                    <a href="#" class="list-group-item list-group-item-action">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h5 class="mb-1">List group item heading</h5>
-                            <small class="text-muted">3 days ago</small>
+                    <a href="#" className="list-group-item list-group-item-action">
+                        <div className="d-flex w-100 justify-content-between">
+                            <h5 className="mb-1">List group item heading</h5>
+                            <small className="text-muted">3 days ago</small>
                         </div>
-                        <p class="mb-1">Some placeholder content in a paragraph.</p>
-                        <small class="text-muted">And some muted small print.</small>
+                        <p className="mb-1">Some placeholder content in a paragraph.</p>
+                        <small className="text-muted">And some muted small print.</small>
                     </a>
                 </div>
-                <div class="list-group w-100 container-md my-5">
-                    <div class="row">
-                    <div class="card col-sm">
-                        <a href="#" class="btn btn-primary">
-                            <img src="..." class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="card col-sm">
-                        <a href="#" class="btn btn-primary">
-                            <img src="..." class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="card col-sm">
-                        <a href="#" class="btn btn-primary">
-                            <img src="..." class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="card col-sm">
-                        <a href="#" class="btn btn-primary">
-                            <img src="..." class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                            </div>
-                        </a>
-                    </div>
+                <div className="list-group w-100 container-md my-5">
+                    <div className="row">
+                        <div className="card col-sm">
+                            <a href="#" className="btn btn-primary">
+                                <img src="..." className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                </div>
+                            </a>
+                        </div>
+                        <div className="card col-sm">
+                            <a href="#" className="btn btn-primary">
+                                <img src="..." className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                </div>
+                            </a>
+                        </div>
+                        <div className="card col-sm">
+                            <a href="#" className="btn btn-primary">
+                                <img src="..." className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                </div>
+                            </a>
+                        </div>
+                        <div className="card col-sm">
+                            <a href="#" className="btn btn-primary">
+                                <img src="..." className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                </div>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
