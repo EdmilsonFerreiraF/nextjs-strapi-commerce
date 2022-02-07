e626e47cf5d7a0f00c4862271f9aacd23544baa3
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-02-07 17:25:46 -0300

    📝 :pencil: [Minor] CC utils - format exp date

--- components/utils.js
+++ components/utils.js
@@ -48,3 +48,13 @@ export function formatCVC(value, prevValue, allValues = {}) {
 
   return clearValue.slice(0, maxLength);
 }
+
+export function formatExpirationDate(value) {
+  const clearValue = clearNumber(value);
+
+  if (clearValue.length >= 3) {
+    return `${clearValue.slice(0, 2)}/${clearValue.slice(2, 4)}`;
+  }
+
+  return clearValue;
+}
