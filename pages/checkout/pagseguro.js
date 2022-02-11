8802c17a32fe5ef1bb9cc021cd866d8d1e48492d
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-02-11 11:45:17 -0300

    🎨 :art: [Refactor] Payment methods encapsulation

--- pages/checkout/pagseguro.js
+++ pages/checkout/pagseguro.js
@@ -18,6 +18,7 @@ import BackToPaymentMethod from '../../components/checkout/backToPaymentMethod';
 import CreditCardMethod from '../../components/checkout/creditCardMethod';
 import ConfirmTab from '../../components/checkout/confirmTab';
 import PaymentTabsControl from '../../components/checkout/paymentTabsControl';
+import PaymentMethods from '../../components/checkout/paymentMethods';
 
 const Checkout = ({ categories }) => {
   const globalContext = useContext(GlobalContext);
@@ -286,40 +287,14 @@ const Checkout = ({ categories }) => {
             }
 
             {paymentMethod === 0 &&
-              <>
-                <div className="align-items-center h-100 text-center row row-cols-2">
-                  <div className="">
-                    <h5 className="mb-4">Cartão de crédito</h5>
-                    <div onClick={() => handlePaymentMethod(1)}>
-                      <img className="rounded mx-auto d-block mw-150px" src="/img/credit_card.png" />
-                    </div>
-                  </div>
-                  <div className="">
-                    <h5 className="mb-4">Cartão de débito</h5>
-                    <div onClick={() => handlePaymentMethod(2)}>
-                      <img className="rounded mx-auto d-block mw-150px" src="/img/debit_card.png" />
-                    </div>
-                  </div>
-                  <div className="">
-                    <h5 className="mb-4">Boleto</h5>
-                    <div onClick={() => handlePaymentMethod(3)}>
-                      <img className="rounded mx-auto d-block mw-150px" src="/img/boleto.png" />
-                    </div>
-                  </div>
-                  <div className="">
-                    <h5 className="mb-4">PIX</h5>
-                    <div onClick={() => handlePaymentMethod(4)}>
-                      <img className="rounded mx-auto d-block mw-150px" src="https://logopng.com.br/logos/pix-106.png" />
-                    </div>
-                  </div>
-                </div>
-                <PaymentTabsControl paymentTab={paymentTab}
-                  disabledNextTab
-                  handlePreviousTab={handlePreviousTab}
-                  handleNextTab={handleNextTab}
-                  handleSubmit={handleSubmit}
-                />
-              </>
+             <PaymentMethods
+             handlePaymentMethod={handlePaymentMethod}
+             paymentTab={paymentTab}
+             disabledNextTab
+             handlePreviousTab={handlePreviousTab}
+             handleNextTab={handleNextTab}
+             handleSubmit={handleSubmit}
+             />
             }
           </div>
         }
