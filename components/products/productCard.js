7169a7f6c9fcadb7855d2ee4f8a4a30957be54e9
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-02-14 12:44:34 -0300

    📝 :pencil: [Minor] Carousel prev button isolation

--- components/products/productCard.js
+++ components/products/productCard.js
@@ -1,11 +1,13 @@
 import React, { useContext } from "react"
 import Link from "next/link"
 import {
-  Button,
+  Button, CarouselItem,
 } from "reactstrap";
 
 import GlobalContext from "../../context/GlobalContext";
 import { getStrapiMedia } from "../../lib/media"
+import CarouselIndicators from "../carousel/carouselIndicators"
+import CarouselPrevButton from "../carousel/carouselPrevButton"
 
 const ProductCard = ({ product }) => {
   const globalContext = useContext(GlobalContext);
@@ -17,28 +19,20 @@ const ProductCard = ({ product }) => {
         <div className="uk-card uk-card-muted">
           <div className="uk-card-media-top">
           <div id="carouselExampleIndicators" className="carousel slide" data-bs-interval="false">
-            <div className="carousel-indicators">
+            {/* <div className="carousel-indicators">
               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
-            </div>
+            </div> */}
+            <CarouselIndicators />
             <div className="carousel-inner">
               <div className="carousel-item active">
                 <img src={getStrapiMedia({ data: product.attributes.image.data[0] })} className="d-block" style={{ height: "450px" }} alt="..." />
               </div>
 
-              {product.attributes.image.data.slice(1).map(image => (
-                <div className="carousel-item">
-                  <img key={image.attributes.id} src={getStrapiMedia({ data: image })} className="d-block" style={{ height: "450px" }} alt="..." />
-                </div>
-              )
-              )}
-
-              <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-              </button>
+              <CarouselItem product={product} />
+              <CarouselPrevButton />
 
               <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
