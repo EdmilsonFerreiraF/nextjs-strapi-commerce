d2e69b7e3dec630b123697a10fed235dce8f6813
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-02-19 15:00:03 -0300

    📝 :pencil: [Minor] Account - Rows encapsulation
    Data fields encapsulation and polymorphism to simplification

--- components/account/row/index.js
+++ components/account/row/index.js
@@ -1,11 +1,11 @@
-const Row = ({ type, editField, value, fieldName }) => {
+const Row = ({ id, handleFocusField, type, editField, value, fieldName }) => {
     return (
         <div class="mb-5 row">
             <label for={`static${fieldName}`} class="col-sm-3 col-form-label">{fieldName}</label>
             <div class="col-sm-8">
-                <input type={type} readOnly={editField !== 1} class={`form-control-plaintext ${editField === 1 ? "focus" : null}`} id={`static${fieldName}`} value={value} />
+                <input type={type} readOnly={editField !== id} class={`form-control-plaintext ${editField === id ? "focus" : null}`} id={`static${fieldName}`} value={value} />
             </div>
-            <i class="bi bi-pencil col" onClick={e => handleFocusField(1, e)}></i>
+            <i class="bi bi-pencil col" onClick={e => handleFocusField(id, e)}></i>
         </div>
     )
 }
--- pages/account.js
+++ pages/account.js
@@ -29,35 +29,10 @@ const Account = ({ categories }) => {
                 <div className="container-md">
                     <h1 className="m-5 text-center">My account</h1>
                     <div className="container-sm">
-                        <Row type="text" editField={editField} value="" fieldName="Name" />
-                        <div class="mb-5 row">
-                            <label for="staticName" class="col-sm-3 col-form-label">Name</label>
-                            <div class="col-sm-8">
-                                <input type="text" readOnly={editField !== 1} class={`form-control-plaintext ${editField === 1 ? "focus" : null}`} id="staticName" value="email@example.com" />
-                            </div>
-                            <i class="bi bi-pencil col" onClick={e => handleFocusField(1, e)}></i>
-                        </div>
-                        <div class="mb-5 row">
-                            <label for="staticEmail" class="col-sm-3 col-form-label">Email</label>
-                            <div class="col-sm-8">
-                                <input type="text" readOnly={editField !== 2} class={`form-control-plaintext ${editField === 2 ? "focus" : null}`} id="staticEmail" value="email@example.com" />
-                            </div>
-                            <i class="bi bi-pencil col" onClick={e => handleFocusField(2, e)}></i>
-                        </div>
-                        <div class="mb-5 row">
-                            <label for="staticPassword" class="col-sm-3 col-form-label">Password</label>
-                            <div class="col-sm-8">
-                                <input type="text" readOnly={editField !== 3} class={`form-control-plaintext ${editField === 3 ? "focus" : null}`} id="staticPassword" value="email@example.com" />
-                            </div>
-                            <i class="bi bi-pencil col" onClick={e => handleFocusField(3, e)}></i>
-                        </div>
-                        <div class="mb-5 row">
-                            <label for="staticBirthDate" class="col-sm-3 col-form-label">Birth date</label>
-                            <div class="col-sm-8">
-                                <input type="text" readOnly={editField !== 4} class={`form-control-plaintext ${editField === 4 ? "focus" : null}`} id="staticBirthDate" value="email@example.com" />
-                            </div>
-                            <i class="bi bi-pencil col" onClick={e => handleFocusField(4, e)}></i>
-                        </div>
+                        <Row type="text" editField={editField} id="1" value="dasd" fieldName="Name" handleFocusField={handleFocusField} />
+                        <Row type="email" editField={editField} id="2" value="gsdfsdf" fieldName="Email" handleFocusField={handleFocusField} />
+                        <Row type="password" editField={editField} id="3" value="gsdfsdf" fieldName="Password" handleFocusField={handleFocusField} />
+                        <Row type="date" editField={editField} id="4" value="gsdfsdf" fieldName="BirthDate" handleFocusField={handleFocusField} />
                     </div>
                 </div>
             </Layout>
