d443d7128f566f73c19232f17acec828afe07d6a
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-02-23 17:06:58 -0300

    🚀 :rocket: [Add] ConfirmPix payment tab

---
+++ components/checkout/pagseguro/paymentTabs/confirm/confirmPix.js
@@ -0,0 +1,14 @@
+const confirmDebitCard = ({ pixFormData }) => {
+    return (
+        <p>
+            <p>Nome: <span className="fw-bold">{pixFormData.nome}</span></p>
+            <p>CPF: <span className="fw-bold">{pixFormData.taxId}</span></p>
+            <p>Total a pagar: <span className="fw-bold">{pixFormData.original}</span></p>
+            <p>Solicitação do pagador: <span className="fw-bold">{pixFormData.solicitacaoPagador}</span></p>
+            <p>InfoAdicionais: <span className="fw-bold">{pixFormData.infoAdicionais}</span></p>
+            <p>Expiração: <span className="fw-bold">{pixFormData.expiracao}</span></p>
+        </>
+    )
+}
+
+export default confirmDebitCard
