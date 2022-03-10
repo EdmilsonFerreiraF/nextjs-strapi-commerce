26efa09101df12d354e3ed097ccb684f19eccbad
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-03-09 23:59:25 -0300

    📝 :pencil: [Minor] Login - forgot pass component

---
+++ components/login/forgotPassword/index.js
@@ -0,0 +1,11 @@
+const ForgotPassword = () => {
+    return (
+        <span>
+        <a href="">
+          <small>Forgot Password?</small>
+        </a>
+      </span>
+    )
+}
+
+export default ForgotPassword
